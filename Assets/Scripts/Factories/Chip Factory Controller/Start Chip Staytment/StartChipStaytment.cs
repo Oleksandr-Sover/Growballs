78038d414408f63e61335bf879a0ed21b2cc73cd
oleksandr_sover <oleksandr.sovertkov@gmail.com>
2023-05-01 20:11:40 +0300

    Revert "StartChipStaytment - Corrected StartingPositionField"
    This reverts commit ca2a268ba29f6f81f0df58908b8367521b0cc997.

--- Assets/Scripts/Factories/Chip Factory Controller/Start Chip Staytment/StartChipStaytment.cs
+++ Assets/Scripts/Factories/Chip Factory Controller/Start Chip Staytment/StartChipStaytment.cs
@@ -5,9 +5,7 @@ namespace GameLogic
     public class StartChipStaytment : MonoBehaviour, IStartChipStaytment
     {
         [SerializeField]
-        Data.LevelData levelData;
-
-        StartingPositionField[] StartingPositionField { get => levelData.StartingPositionField; }
+        StartingPositionField[] startingPositionField;
 
         [SerializeField]
         float minScale;
@@ -18,7 +16,7 @@ namespace GameLogic
 
         void Start()
         {
-            fieldCount = StartingPositionField.Length;
+            fieldCount = startingPositionField.Length;
         }
 
         public void SetChipStaytment(GameObject chip)
@@ -33,11 +31,11 @@ namespace GameLogic
 
             if (fieldCount == 1)
             {
-                StartingPositionField[0].SetMaxYPosition(numOfChipsPerField, minScale, maxScale);
+                startingPositionField[0].SetMaxYPosition(numOfChipsPerField, minScale, maxScale);
             }
             else if (fieldCount > 1)
             {
-                foreach (var field in StartingPositionField)
+                foreach (var field in startingPositionField)
                 {
                     field.SetMaxYPosition(numOfChipsPerField, minScale, maxScale);
                 }
@@ -52,17 +50,17 @@ namespace GameLogic
             }
             else if (fieldCount > 1)
             {
-                int fieldNumber = Random.Range(0, StartingPositionField.Length);
+                int fieldNumber = Random.Range(0, startingPositionField.Length);
                 SetRandomPosition(chip, fieldNumber);
             }
         }
 
         void SetRandomPosition(GameObject chip, int fieldNumber)
         {
-            float minX = StartingPositionField[fieldNumber].minXPosition;
-            float maxX = StartingPositionField[fieldNumber].maxXPosition;
-            float minY = StartingPositionField[fieldNumber].minYPosition;
-            float maxY = StartingPositionField[fieldNumber].maxYPosition;
+            float minX = startingPositionField[fieldNumber].minXPosition;
+            float maxX = startingPositionField[fieldNumber].maxXPosition;
+            float minY = startingPositionField[fieldNumber].minYPosition;
+            float maxY = startingPositionField[fieldNumber].maxYPosition;
             float xPosition = Random.Range(minX, maxX);
             float yPosition = Random.Range(minY, maxY);
             Vector2 position = new Vector2(xPosition, yPosition);
@@ -76,33 +74,33 @@ namespace GameLogic
             chip.transform.localScale = scaler;
         }
 
-        //void OnDrawGizmos()
-        //{
-        //    if (startingPositionField.Length > 0) 
-        //    { 
-        //        Gizmos.color = Color.magenta;
-        //        Vector3 fromPos;
-        //        Vector3 toPos;
+        void OnDrawGizmos()
+        {
+            if (startingPositionField.Length > 0) 
+            { 
+                Gizmos.color = Color.magenta;
+                Vector3 fromPos;
+                Vector3 toPos;
 
-        //        foreach (var field in startingPositionField)
-        //        {
-        //            fromPos = new Vector3(field.minXPosition, field.minYPosition, 0);
-        //            toPos = new Vector3(field.maxXPosition, field.minYPosition, 0);
-        //            Gizmos.DrawLine(fromPos, toPos);
+                foreach (var field in startingPositionField)
+                {
+                    fromPos = new Vector3(field.minXPosition, field.minYPosition, 0);
+                    toPos = new Vector3(field.maxXPosition, field.minYPosition, 0);
+                    Gizmos.DrawLine(fromPos, toPos);
 
-        //            fromPos = new Vector3(field.minXPosition, field.minYPosition, 0);
-        //            toPos = new Vector3(field.minXPosition, field.maxYPosition, 0);
-        //            Gizmos.DrawLine(fromPos, toPos);
+                    fromPos = new Vector3(field.minXPosition, field.minYPosition, 0);
+                    toPos = new Vector3(field.minXPosition, field.maxYPosition, 0);
+                    Gizmos.DrawLine(fromPos, toPos);
 
-        //            fromPos = new Vector3(field.maxXPosition, field.minYPosition, 0);
-        //            toPos = new Vector3(field.maxXPosition, field.maxYPosition, 0);
-        //            Gizmos.DrawLine(fromPos, toPos);
+                    fromPos = new Vector3(field.maxXPosition, field.minYPosition, 0);
+                    toPos = new Vector3(field.maxXPosition, field.maxYPosition, 0);
+                    Gizmos.DrawLine(fromPos, toPos);
 
-        //            fromPos = new Vector3(field.minXPosition, field.maxYPosition, 0);
-        //            toPos = new Vector3(field.maxXPosition, field.maxYPosition, 0);
-        //            Gizmos.DrawLine(fromPos, toPos);
-        //        }
-        //    }
-        //}
+                    fromPos = new Vector3(field.minXPosition, field.maxYPosition, 0);
+                    toPos = new Vector3(field.maxXPosition, field.maxYPosition, 0);
+                    Gizmos.DrawLine(fromPos, toPos);
+                }
+            }
+        }
     }
 }
